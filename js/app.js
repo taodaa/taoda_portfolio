cc5dceb03c29a59235a25eb57d4b70d3e44f15cc
taodaa <philipeidolon@vk.com>
2022-05-03 22:35:58 +0500

    v14

--- js/app.js
+++ js/app.js
@@ -14,12 +14,10 @@ function draw() {
     div.id = "p" + step;
     div.classList.add('slider__slide');
     div.style.top = offset * 512 + 'px';
-    console.log(div);
     document.querySelector('.slider').appendChild(div);
 
     if (step + 1 == slides.length) {
         step = 0
-        // console.log(s)
     }
     else {
         step++
@@ -33,17 +31,15 @@ function left() {
     let offset2 = 0;
     for (let i = 0; i < slides2.length; i++) {
         slides2[i].style.top = offset2 * 512 - 512 + 'px';
+        slides2[i].style.animation = 'twiling' + ' 1s';
         offset2++;
     }
-
     setTimeout(() => {
         slides2[0].remove();
-        console.log(slides2);
         document.onclick = left;
         draw();
     }, 1000);
 }
 
 draw(); draw();
-
 document.onclick = left;
