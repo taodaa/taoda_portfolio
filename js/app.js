15cbc52a946342ee3f059dc0407a8257b30cca04
taodaa <philipeidolon@vk.com>
2022-05-03 19:41:37 +0500

    м12

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function draw() {
     div.style.top = offset * 512 + 'px';
     console.log(div);
     document.querySelector('.slider').appendChild(div);
+
     if (step + 1 == slides.length) {
         step = 0
         // console.log(s)
@@ -24,6 +25,15 @@ function draw() {
         step++
     }
     offset = 1;
+    // function opacity() {
+    //     if (div.style.top === 512 + 'px') {
+    //         div.style.animation = 'blink';
+    //     }
+    //     else if (div.style.top == 0 + 'px') {
+    //         div.style.animation = 'none';
+    //     }
+    // }
+
 }
 function left() {
     document.onclick = null
